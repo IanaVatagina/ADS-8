d2a5ab237cd7606064db23192eb40ccf49f37128
IanaVatagina <150849744+IanaVatagina@users.noreply.github.com>
2024-05-18 14:32:00 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,43 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+void Train::addCage(bool light) {
+  if (first->prev != nullptr) {
+    Cage *cTemp = first->next;
+    first->next = new Cage;
+    first = first->next;
+    first->next->prev = first;
+    first->next = temp;
+    first->light = light;
+    temp->prev = first;
+  } else if (first->prev == nullptr) {
+    first->prev = first;
+    first->next = first;
+    first-> light = light;
+  }
+}
+
+int Train::getLength() {
+  first->light = true;
+  while (true) {
+    countOp = countOp + 1;
+    while (first->next->light == false) {
+      countOp = countOp + 1;
+      iLen = iLen + 1;
+      first = first->next;
+    }
+    countOp = countOp + 1;
+    first->next->light = false;
+    iRes = iLen + 1;
+    for (; iLen > 0; iLen--) {
+      first = first->prev;
+      countOp++;
+    }
+    if (first->next->light == false)
+      break;
+  }
+  return iRes;
+}
+
+int Train::getOpCount() {
+  return countOp();
+}
